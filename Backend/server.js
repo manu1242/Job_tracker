dacde4c8c774a66b27ad3b84f3e5dcc0a33259dc
manu1242 <140734382+manu1242@users.noreply.github.com>
2025-04-09 16:06:42 +0530

    Update server.js
    upadte

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import jobRoutes from './routes/jobroutes.js'
+import jobRoutes from './routes/jobRoutes.js'
 dotenv.config()
 const app = express()
 
